ac388b391025824b7136cb4f0b60d21b0d51d882
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 14:09:11 +0200

    converted class component Game to functional component

--- src/components/Square/Game/Game.js
+++ src/components/Square/Game/Game.js
@@ -1,26 +1,23 @@
-import { Component } from "react";
 import Row from "../Row/Row";
 import Cell from "../Row/Cell/Cell";
 
-class Game extends Component {
-  render() {
-    return (
-      <div className="grid">
-        {this.props.matrixRand.map((row, ri) => (
-          <Row key={ri}>
-            {row.map((cellValue, ci) => (
-              <Cell
-                key={`${ri}-${ci}`}
-                indexRow={ri}
-                indexCol={ci}
-                matrixRand={this.props.matrixRand}
-              />
-            ))}
-          </Row>
-        ))}
-      </div>
-    );
-  }
-}
+const Game = (props) => {
+  return (
+    <div className="grid">
+      {props.matrixRand.map((row, ri) => (
+        <Row key={ri}>
+          {row.map((cellValue, ci) => (
+            <Cell
+              key={`${ri}-${ci}`}
+              indexRow={ri}
+              indexCol={ci}
+              matrixRand={props.matrixRand}
+            />
+          ))}
+        </Row>
+      ))}
+    </div>
+  );
+};
 
 export default Game;
